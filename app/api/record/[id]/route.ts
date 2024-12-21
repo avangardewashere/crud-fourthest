457d81dc3c83cf29db2bf8f020d5c3e37339c7d9
avangardewashere <avelpanaligan@gmail.com>
2024-12-21 15:29:27 +0800

    add/get-id-and put-route-hadnler

--- app/api/record/[id]/route.ts
+++ app/api/record/[id]/route.ts
@@ -1,5 +1,28 @@
+import connectMongoDb from "@/libs/mongodb";
+import Record from "@/models/record";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  return NextResponse.json({ request: "success" });
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const { newRecord: record, newDescription: description } =
+    await request.json();
+  await connectMongoDb();
+  // find the id and update it with the submitted new data
+  await Record.findByIdAndUpdate(id, { record, description });
+
+  return NextResponse.json({ message: "The record has been updated" });
+}
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  await connectMongoDb();
+  const record = await Record.findOne({ _id: id });
+
+  return NextResponse.json({ record  },{ status:200});
 }
