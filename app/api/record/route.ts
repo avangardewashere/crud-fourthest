326261eaa871487250c4c2cc1b4be47f5384b428
avangardewashere <avelpanaligan@gmail.com>
2024-12-21 15:17:50 +0800

    add/get-delete-route-handlers

--- app/api/record/route.ts
+++ app/api/record/route.ts
@@ -2,24 +2,28 @@ import connectMongoDb from "@/libs/mongodb";
 import Record from "@/models/record";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request:NextRequest){
+export async function POST(request: NextRequest) {
+  const { record, description } = await request.json();
+  await connectMongoDb();
+  await Record.create({ record, description });
 
-    const {record,description} = await request.json();
-    await connectMongoDb();
-    await Record.create({record,description});
-
-    return NextResponse.json({message:"Record Submission Success",data:{record,description}})
-
-
+  return NextResponse.json({
+    message: "Record Submission Success",
+    data: { record, description },
+  });
 }
 
-export async function GET(request:NextRequest){
+export async function GET() {
+  await connectMongoDb();
+  const records = await Record.find();
 
-    // const {record,description} = await request.json();
-    // const res = await connectMongoDb();
-    // await Record.create({record,description});
+  return NextResponse.json({ data: records });
+}
 
-    return NextResponse.json({message:"Hello"})
+export async function DELETE(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDb();
+  await Record.findByIdAndDelete(id);
 
-
+  return NextResponse.json({ message: "Record has been deleted" });
 }
