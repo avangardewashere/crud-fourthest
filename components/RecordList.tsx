ffb2ab072a22f11afe22e885518dd1b13f11c07b
avangardewashere <avelpanaligan@gmail.com>
2024-12-21 13:14:48 +0800

    add/record-card

--- components/RecordList.tsx
+++ components/RecordList.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import RemoveButton from "./RemoveButton";
+import Link from "next/link";
+import { HiPencilAlt } from "react-icons/hi";
 
-export default  function RecordList() {
+export default function RecordList() {
   return (
-    <div>
-      Topic list
+    <div className="flex shadow-sm justify-between p-6 py-4 bg-slate-50 border-opacity-75 border-sky-300 border rounded-md my-2 hover:bg-white">
+      <div>
+        <h2>Unit Records</h2>
+        <div>Record Description</div>
+      </div>
+
+      <div className="flex gap-2 items-center">
+        <RemoveButton />
+        <Link className="text-green-400" href={"/updateRecord/123"}>
+          <HiPencilAlt size={24} />
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
--- components/RemoveButton.tsx
+++ components/RemoveButton.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { HiTrash } from "react-icons/hi2";
 
-export default  function RemoveButton() {
-  
+export default function RemoveButton() {
   const handleRemove = () => {
-    alert("about to remove")
-  }
-    return (
+    alert("about to remove");
+  };
+  return (
     <button onClick={handleRemove}>
-      
+      <HiTrash size={22} />
     </button>
-  )
+  );
 }
