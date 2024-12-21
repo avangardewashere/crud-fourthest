ac4241a73b1ddfbe4b0cd5e794e30f25d0d7a592
avangardewashere <avelpanaligan@gmail.com>
2024-12-21 15:43:56 +0800

    implement/get-route-handling-front-end

--- components/RecordList.tsx
+++ components/RecordList.tsx
@@ -3,20 +3,50 @@ import RemoveButton from "./RemoveButton";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function RecordList() {
-  return (
-    <div className="flex shadow-sm justify-between p-6 py-4 bg-slate-50 border-opacity-75 border-sky-300 border rounded-md my-2 hover:bg-white">
-      <div>
-        <h2>Unit Records</h2>
-        <div>Record Description</div>
-      </div>
+interface IRecordItem {
+  _id: string;
+  record: string;
+  description: string;
+}
 
-      <div className="flex gap-2 items-center">
-        <RemoveButton />
-        <Link className="text-green-400" href={"/updateRecord/123"}>
-          <HiPencilAlt size={24} />
-        </Link>
-      </div>
-    </div>
+const getRecord = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/record", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch the data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Encountered an error: ", error);
+  }
+};
+export default async function RecordList() {
+  const { data:recordList} = await getRecord();
+
+  return (
+    <>
+      {recordList?.map((recordItem: IRecordItem) => (
+        <div className="flex shadow-sm justify-between p-6 py-4 bg-slate-50 border-opacity-75 border-sky-300 border rounded-md my-2 hover:bg-white">
+          <div>
+            <h2>{recordItem.record ?? "Record Idenitifier"}</h2>
+            <div>{recordItem.description ?? "Record Description"}</div>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <RemoveButton />
+            <Link
+              className="text-green-400"
+              href={"/updateRecord/" + recordItem._id}
+            >
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
